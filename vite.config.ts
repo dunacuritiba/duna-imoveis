014dba11c4f6b707f30007353a7c0f87616bb9f8
dunacuritiba <dunacuritiba@gmail.com>
2026-09-09 13:11:01 -0300

    vite config adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/duna-imoveis/', // Tem que ter a barra antes e depois
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: '/duna-imoveis/',
 })
